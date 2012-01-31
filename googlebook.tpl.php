c7cbe2e007868693b66c2a52844387186797f34e
Darrell Ulm <darrellulm@gmail.com>
2012-01-30 22:34:09 -0500

    Cleaned up code not used, added security

--- googlebook.tpl.php
+++ googlebook.tpl.php
@@ -1,26 +1,22 @@
-<div id="googlebook">
-
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * @file googlebook.tpl.php
+ *
+ * This is the default template file for the googlebook filter.
+ *
  */
 
+print '<div class="googlebook">';
 
-
+// Get the data array
 $bib = googlebookapi_bib_field_array();
-
-
-
-
-//drupal_add_js($googlebook_js_string, 'inline');
 
 // Build the main title with a link.
 if (($title_link == 1 || $title_link === "") && isset($infolink) && isset($title)) {
   print '<div class="googlebooktitle">' . t('Title') . ': <a href="' . htmlentities($infoLink) . '" rel="nofollow" target="_blank"><i>' . $title . '</i></a></div>';
 }
 
-// Get the book links if any are found. None are checked for ISBN validity.
+// Show the book links if any are found. None are checked for ISBN validity.
 if (!empty($isbn) || TRUE) {
   if ($worldcat_link) {
     $worldcat = '<a href="http://worldcat.org/isbn/' . $isbn . '" rel="nofollow" target="_blank">WorldCat</a><br />';
@@ -36,7 +32,7 @@ if (!empty($isbn) || TRUE) {
   }
 }
 
-// Build a coverimage.
+// Build a coverimage if user selected.
 if (isset($thumbnail) && ($image_option == 1 || $image_on == 1) && $image_on !== 0) {
   $img_link = $thumbnail;
   if ($page_curl == 1) {
@@ -47,25 +43,26 @@ if (isset($thumbnail) && ($image_option == 1 || $image_on == 1) && $image_on !==
     $img_link = str_replace("&edge=curl", "", $img_link);
   }
   $html_coverimage = "<img class='googlebookimage' src='" . htmlentities($img_link) . "' alt='" . $title . "' height='" . $image_height . "' width='" . $image_width . "' />";
-
   print "<a href='" . htmlentities($infoLink) . "' rel='nofollow' target='_blank'>" . $html_coverimage . "</a>";
 }
 
-if (isset($google_viewer)){
+// Show the google book viewer if selected and isbn exists.
+if (!empty($googlebook_js_string)){
   print "<script type='text/javascript' src='".GOOGLE_BOOK_EXTERN_JS."'></script>";
   print "<script type='text/javascript'>".$googlebook_js_string."</script>";
+  $google_viewer = '<div class="googleviewer" id="viewerCanvas' . $isbn . '" style = "width:' . $reader_width . 'px; height:' . $reader_height . 'px"></div><p></p><br />';
   print $google_viewer;
 }
+
 // Output the biblio fields in the array.
 print "<ul>";
 foreach ($bib as $bib_index) {
   if (isset (${$bib_index})) {
     print "<li>" . drupal_ucfirst(preg_replace('/[A-Z]/', ' $0', str_replace('_', ' ', $bib_index))) . ": " . googlebook_make_html_link(${$bib_index}) . "</li>";
-    //print "<li>" . $bib_index . ": " . ${$bib_index} . "</li>";
   }
 }
 print "</ul>";
+print "</div>";
 
+//drupal_add_js(GOOGLE_BOOK_EXTERN_JS,'theme','external','header');
 ?>
-
-</div>
