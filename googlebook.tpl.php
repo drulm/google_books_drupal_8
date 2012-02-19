693c9e9840fa9df4d43d7428e0767598232bcabb
Darrell Ulm <darrellulm@gmail.com>
2012-02-19 15:50:44 -0500

    Issue #1387232 by darrellulm: Made several fixes to simplify templating.

--- googlebook.tpl.php
+++ googlebook.tpl.php
@@ -10,54 +10,57 @@
  */
 ?>
 
+<!-- Main CSS of a Google Book entry, multiple per page. -->
 <div class="googlebook">
-  
-<?php
-// Build the main title with a link.
-if ($title_link !== 0 && isset($info_link) && isset($title)):
-  print '<div class="googlebooktitle"><i>' . l($title, check_url($info_link), array('attributes' => array('rel' => 'nofollow', 'target' => '_blank'))) . '</i></a></div>';
-endif;
 
-// Show the book links if any are found. None are checked for ISBN validity.
-if (!empty($isbn) || TRUE):
-  if ($worldcat_link):
-    $worldcat = '<a href="http://worldcat.org/isbn/' . $isbn . '" rel="nofollow" target="_blank">WorldCat</a><br />';
+  <!-- Print the title of the book. -->
+  <div class="googlebook_title">
+      <?php print $title_anchor; ?>
+  </div>
+
+  <!-- Build image theme function call for book cover. -->
+  <div class="googlebook_image">
+    <?php
+      if ($img_link != ""):
+      print theme('image', array(
+        'path' => $img_link,
+        'alt' => $title,
+        'title' => $title,
+        'width' => $image_width,
+        'height' => $image_height,
+        'attributes' => array(
+          'rel' => 'nofollow',
+          'target' => '_blank',
+        ),
+      ));
+      endif;
+    ?>
+  </div>
+
+  <!-- Display links and leave empty divs for theming. -->
+  <div class="googlebook_links">
+    <div class="googlebook_worldcat">
+      <?php print $worldcat; ?>
+    </div>
+    <div class="googlebook_librarything">
+      <?php print $librarything; ?>
+    </div>
+    <div class="googlebook_openlibrary">
+      <?php print $openlibrary; ?>
+    </div>
+  </div>
+
+  <!-- Show the Google book viewer if needed. -->
+  <!-- Embed direct because of the filter JavaScript issue in Drupal -->
+  <?php
+  if (!empty($googlebook_js_string)):
+    print "<script type='text/javascript'>" . $googlebook_js_string . "</script>";
+    print '<div class="googleviewer" id="viewerCanvas' . $isbn . '" style = "width:' . $reader_width . 'px; height:' . $reader_height . 'px"></div><p></p><br />';
   endif;
-  if ($librarything_link):
-    $librarything = '<a href="http://librarything.com/isbn/' . $isbn . '" rel="nofollow" target="_blank">LibraryThing</a><br />';
-  endif;
-  if ($openlibrary_link):
-    $openlibrary = '<a href="http://openlibrary.org/isbn/' . $isbn . '" rel="nofollow" target="_blank">Open Library</a><br />';
-  endif;
-  if (drupal_strlen($worldcat . $librarything . $openlibrary) > 0):
-    print '<div class="googlebooklinks">' . t("Link to") . ":<br />" . $worldcat . $librarything . $openlibrary . '</div>';
-  endif;
-endif;
+  ?>
 
-// Build a coverimage if user selected.
-if ($img_link != ""):
-  $html_coverimage = "<img class='googlebookimage' src='" . htmlentities($img_link) . "' alt='" . $title . "' height='" . $image_height . "' width='" . $image_width . "' />";
-  print "<a href='" . htmlentities($info_link) . "' rel='nofollow' target='_blank'>" . $html_coverimage . "</a>";
-endif;
-
-// Show the google book viewer if selected and isbn exists.
-if (!empty($googlebook_js_string)):
-  print "<script type='text/javascript' src='" . GOOGLE_BOOK_EXTERN_JS . "'></script>";
-  print "<script type='text/javascript'>" . $googlebook_js_string . "</script>";
-  $google_viewer = '<div class="googleviewer" id="viewerCanvas' . $isbn . '" style = "width:' . $reader_width . 'px; height:' . $reader_height . 'px"></div><p></p><br />';
-  print $google_viewer;
-endif;
-?>
-
-<ul>
-
-<?php
-// Output the biblio fields in the array.
-foreach ($bib as $bib_index):
-  if (isset ($$bib_index)):
-    print "<li>" . drupal_ucfirst(preg_replace('/[A-Z]/', ' $0', str_replace('_', ' ', $bib_index))) . ": " . googlebook_make_html_link($$bib_index) . "</li>";
-  endif;
-endforeach;
-?>
-
-</ul></div>
+  <!-- List the selected and available Google Book data fields. -->
+  <?php
+  print theme('googlebookbiblio', $selected_bibs);
+  ?>
+</div>
