fb1307ff56ba2fd171bb96aaa8e86d624ffe83c5
Darrell Ulm <darrellulm@gmail.com>
2012-02-28 12:59:43 -0500

    Issue #1387232 by darrellulm: Fixed documentation for hook functions and expanded theme documentation to document the passed variables. Also increased biblio select list size.

--- google_books.tpl.php
+++ google_books.tpl.php
@@ -1,12 +1,24 @@
 <?php
 /**
  * @file
- * Google Books Template Drupal 7.0
+ * Displays a Google Books item.
  *
- * @author Darrell Ulm.
+ * Available variables:
+ * - $title_anchor: The pre-generated achor tag with the title.
+ * - $book_image: Pre-generated Google Books image tag.
+ * - $worldcat: The pre-generated anchor tag with a WorldCat link.
+ * - $librarything: The pre-generated anchor tag for Library Thing.
+ * - $openlibrary: The pre-generated anchor tag for Open Library.
+ * - $google_books_js_string: Google Books reader javascript string.
+ * - $isbn: The book ISBN, if it exists. It should if $google_books_js_string.
+ * - $reader_width: Width in pixels of Google Reader.
+ * - $reader_height: Height in pixels of Google Reader.
+ * - $bib_data: Pre-generated list of biblio fields.
  *
- * This is the default template file for the google_book filter.
+ * @see google_books_preprocess_google_books_aggregate()
+ * @see theme_google_books_biblio()
  *
+ * @ingroup themeable
  */
 ?>
 
@@ -36,7 +48,7 @@
     <?php endif; ?>
     <?php if ($librarything): ?>
       <span class="google_books_librarything">
-        <?php print $librarything . "&nbsp;" ; ?>
+        <?php print $librarything . "&nbsp;"; ?>
       </span>
     <?php endif; ?>
     <?php if ($librarything): ?>
