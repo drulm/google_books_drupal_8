919036a896260064cf3f7afe50fe6899cdc4c881
drulm <darrellulm@gmail.com>
2015-09-19 14:52:33 -0400

    Issue #1925808 by darrell_ulm: Values saved in filter forms working.

--- src/Plugin/Filter/Googlebooks.php
+++ src/Plugin/Filter/Googlebooks.php
@@ -28,37 +28,40 @@ class Googlebooks extends FilterBase {
    */
   public function settingsForm(array $form, FormStateInterface $form_state) {
 
-    $input = &$form_state->getUserInput(); 
+    /*$input = &$form_state->getUserInput(); 
     $input = $input['filters']['google_books']['settings'];
     dpm($input);
-    
+    */
     $form['api_key'] = array(
       '#type' => 'textfield',
       '#title' => t('Google Books API Key'),
       '#size' => 60,
       '#maxlength' => 80,
       '#description' => t('Register your key at: (fill in link later)'),
-      '#default_value' => isset($input['api_key']) ? $input['api_key'] : "",
+      '#default_value' => $this->settings['api_key'],
     );
     $form['worldcat'] = array(
       '#type' => 'checkbox',
       '#title' => t('Link to WorldCat'),
-      '#default_value' => isset($input['worldcat']) ? $input['worldcat'] : FALSE,
+      '#default_value' => $this->settings['worldcat'],
     );
     $form['librarything'] = array(
       '#type' => 'checkbox',
       '#title' => t('Link to LibraryThing'),
-      '#default_value' => isset($input['librarything']) ? $input['librarything'] : FALSE,
+      '#default_value' => $this->settings['librarything'],
+      //'#default_value' => isset($input['librarything']) ? $input['librarything'] : FALSE,
     );
     $form['openlibrary'] = array(
       '#type' => 'checkbox',
       '#title' => t('Link to Open Library'),
-      '#default_value' => isset($input['openlibrary']) ? $input['openlibrary'] : FALSE,
+      '#default_value' => $this->settings['openlibrary'],
+      //'#default_value' => isset($input['openlibrary']) ? $input['openlibrary'] : FALSE,
     );
     $form['image'] = array(
       '#type' => 'checkbox',
       '#title' => t('Include Google Books cover image'),
-      '#default_value' => isset($input['image']) ? $input['image'] : FALSE,
+      '#default_value' => $this->settings['image'],
+      //'#default_value' => isset($input['image']) ? $input['image'] : FALSE,
     );
     $form['image_height'] = array(
       '#type' => 'textfield',
@@ -68,7 +71,8 @@ class Googlebooks extends FilterBase {
       // @TODO Fix validator
       // '#element_validate' => array('_google_books_image_or_reader_valid_int_size'),
       '#description' => t('Height of Google cover image'),
-      '#default_value' => isset($input['image_height']) ? $input['image_height'] : FALSE,
+      '#default_value' => $this->settings['image_height'],
+      //'#default_value' => isset($input['image_height']) ? $input['image_height'] : FALSE,
     );
     $form['image_width'] = array(
       '#type' => 'textfield',
@@ -78,12 +82,14 @@ class Googlebooks extends FilterBase {
       // @TODO Fix validator
       // '#element_validate' => array('_google_books_image_or_reader_valid_int_size'),
       '#description' => t('Width of Google cover image'),
-      '#default_value' => isset($input['image_width']) ? $input['image_width'] : FALSE,
+      '#default_value' => $this->settings['image_width'],
+      //'#default_value' => isset($input['image_width']) ? $input['image_width'] : FALSE,
     );
     $form['reader'] = array(
       '#type' => 'checkbox',
       '#title' => t('Include the Google Books reader'),
-      '#default_value' => isset($input['reader']) ? $input['reader'] : FALSE,
+      '#default_value' => $this->settings['reader'],
+      //'#default_value' => isset($input['reader']) ? $input['reader'] : FALSE,
     );
     $form['reader_height'] = array(
       '#type' => 'textfield',
@@ -93,7 +99,8 @@ class Googlebooks extends FilterBase {
       // @TODO Fix validator
       //'#element_validate' => array('_google_books_image_or_reader_valid_int_size'),
       '#description' => t('Height of Google reader'),
-      '#default_value' => isset($input['reader_height']) ? $input['reader_height'] : FALSE,
+      '#default_value' => $this->settings['reader_height'],
+      //'#default_value' => isset($input['reader_height']) ? $input['reader_height'] : FALSE,
     );
     $form['reader_width'] = array(
       '#type' => 'textfield',
@@ -103,7 +110,8 @@ class Googlebooks extends FilterBase {
       // @TODO Fix validator
       //'#element_validate' => array('_google_books_image_or_reader_valid_int_size'),
       '#description' => t('Width of Google reader'),
-      '#default_value' => isset($input['reader_width']) ? $input['reader_width'] : FALSE,
+      '#default_value' => $this->settings['reader_width'],
+      //'#default_value' => isset($input['reader_width']) ? $input['reader_width'] : FALSE,
     );
     return $form;
   }
